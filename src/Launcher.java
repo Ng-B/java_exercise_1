6758bf90e6fc53a478daa779d34b921a38439e88
Naod Bekele <naod.bekele@epita.fr>
2022-02-10 15:10:58 +0100

    Partie 5 - freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,10 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.nio.file.Files.*;
+import java.util.stream.Collectors;
 
 public class Launcher {
 
@@ -7,6 +13,30 @@ public class Launcher {
         if (num <= 1)
             return num;
         return displayFibonacci(num-1) + displayFibonacci(num-2);
+    }
+
+    static  void frequency(String[] content) {
+        HashMap<String, Integer> assoc = new HashMap<String, Integer>();
+
+        for (String str : content) {
+            if (assoc.containsKey(str)) {
+                assoc.put(str, assoc.get(str) + 1);
+            } else {
+                assoc.put(str, 0);
+            }
+        }
+
+        Map<String, Integer> result = assoc.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        List<String> list3 = new ArrayList<>(result.keySet());
+        String res = String.join(" ", list3);
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
@@ -22,14 +52,32 @@ public class Launcher {
             {
                 System.out.println("Nombre à utiliser ?");
                 Integer num = scn.nextInt();
-
                 //FIBONACCI
                 System.out.println(displayFibonacci(num));
             }
-            else
+            else if (inp.equals("freq"))
             {
-                System.out.println("Unknown command");
+                System.out.println("Emplacement du fichier ?");
+                String path = scn.nextLine();
+
+                Path filePath = Paths.get(path);
+                try
+                {
+                    String content = Files.readString(filePath);
+                    String[] spl_content = content.split(" ");
+                    frequency(spl_content);
+                    System.out.println(content);
+                }
+                catch (IOException e)
+                {
+                    System.out.println("Unreadable file: " + path);
+                }
+
+
             }
+            else
+                System.out.println("Unknown command");
+
             inp = scn.nextLine();
         }
     }
