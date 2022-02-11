cf9934dad606448e3dae66c300fb131a2cd62f8f
Naod Bekele <naod.bekele@epita.fr>
2022-02-11 11:18:55 +0100

    Current fix : freq

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,6 +5,14 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.nio.file.Files.*;
 import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Launcher {
 
@@ -13,6 +21,39 @@ public class Launcher {
         if (num <= 1)
             return num;
         return displayFibonacci(num-1) + displayFibonacci(num-2);
+    }
+
+    static Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap,  boolean order)
+    {
+
+        List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<String, Integer>>()
+        {
+            public int compare(Entry<String, Integer> o1,
+                               Entry<String, Integer> o2)
+            {
+                if (order)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Entry<String, Integer> entry : list)
+        {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedMap;
     }
 
     static  void frequency(String[] content) {
@@ -26,16 +67,21 @@ public class Launcher {
             }
         }
 
-        Map<String, Integer> result = assoc.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-        List<String> list3 = new ArrayList<>(result.keySet());
-        String res = String.join(" ", list3);
+
+        Map<String, Integer> sortedMapDesc = sortByComparator(assoc, false);
+
+        Integer i = 0;
+        String res = "";
+
+        for (String key : sortedMapDesc.keySet())
+        {
+            if (i == 3)
+                break;
+            res = res.concat(key+ " ") ;
+            i++;
+        }
+
         System.out.println(res);
     }
 
@@ -64,9 +110,8 @@ public class Launcher {
                 try
                 {
                     String content = Files.readString(filePath);
-                    String[] spl_content = content.split(" ");
+                    String[] spl_content = content.toLowerCase().split(" ");
                     frequency(spl_content);
-                    System.out.println(content);
                 }
                 catch (IOException e)
                 {
